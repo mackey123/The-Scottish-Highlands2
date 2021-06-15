4594d403ad42cfb315f8704c50e7287203c1b6a7
mackey123 <seanrobert2010@gmail.com>
2021-06-15 00:04:06 +0000

    add gallery and style with css and js, ad djs to all headers so that color is changed when hovered over, re-style about section to fit better and change background color to a blue instead of greenish blue, add a check time button under the gallery section

---
+++ assets/script.js
@@ -0,0 +1,51 @@
+
+// adapted from CSS ticks (J-Query)
+
+// $("#slideshow > div:gt(0)").hide();
+// setInterval(function() { 
+//   $('#slideshow > div:first')
+//   .fadeOut(1000)
+//   .next()
+//   .fadeIn(1000)
+//   .end()
+//   .appendTo('#slideshow');
+// }, 3000);
+
+
+
+
+
+// Gallery script
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous button controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+//  image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot") ;
+  if (n > slides.length) {slideIndex = 1}
+
+  if (n < 1) {slideIndex = slides.length}
+
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none" ;
+  }
+
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
